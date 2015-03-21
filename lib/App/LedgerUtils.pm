1de728a659d0ccd1a5ee8d188194e08fe08b6d86
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-03-21 12:28:15 +0700

    Release v0.00

---
+++ lib/App/LedgerUtils.pm
@@ -0,0 +1,30 @@
+package App::LedgerUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Command-line utilities related Ledger files
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities related to
+Ledger files:
+
+#INSERT_EXECS_LIST
+
+The main purpose of these utilities is tab completion.
+
+
+=head1 FAQ
+
+#INSERT_BLOCK: App::PMUtils faq
+
+
+=head1 SEE ALSO
+
+#INSERT_BLOCK: App::PMUtils see_also
+
+=cut

fcf82a47518499bb375adc04c215ce856c7a35b0
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-26 19:25:46 +0700

    Add script: parse-ledger

--- lib/App/LedgerUtils.pm
+++ lib/App/LedgerUtils.pm
@@ -5,6 +5,27 @@ package App::LedgerUtils;
 
 use 5.010001;
 
+our %common_args = (
+    ledger => {
+        summary => 'Ledger file',
+        schema  => 'str*',
+        req     => 1,
+        pos     => 0,
+        cmdline_src => 'stdin_or_file',
+        tags    => ['common'],
+    },
+    # XXX add parser configuration arguments
+);
+
+sub _get_parser {
+    require Ledger::Parser;
+
+    my $args = shift;
+    Ledger::Parser->new(
+        # XXX add parser configuration arguments
+    );
+}
+
 1;
 # ABSTRACT: Command-line utilities related Ledger files
 
